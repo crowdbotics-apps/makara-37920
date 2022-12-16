bd17860d44b6028b5c5d4c1d882be87db642df01
Crowdbotics <team@crowdbotics.com>
2022-12-16 16:18:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "makara_37920",
+  url: "https://makara_37920.botics.co",
+  api: "https://makara_37920.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
